96cc80502e925867581c9e1e6d71dd11238bfc8d
ichiyo <ichiyo@ingstar.moe>
2025-03-14 19:55:23 +0800

    feat: 迁移至 Next.js

--- . lintstagedrc.js
+++ . lintstagedrc.js
@@ -1,11 +1,7 @@
 import path from 'path';
- 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
- 
+
+const buildEslintCommand = (filenames) => `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  "*.{js,jsx,ts,tsx,md,html,css,scss}": "prettier --write"
-}
+	'*.{js,jsx,ts,tsx}': [buildEslintCommand],
+};
