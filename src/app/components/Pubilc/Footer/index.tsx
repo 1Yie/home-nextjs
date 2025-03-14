08b819bf2421ecb7ae1c07446b8ff28f9769fcd8
ichiyo <ichiyo@ingstar.moe>
2025-03-15 01:36:58 +0800

    feat: 调整 Footer logo 颜色

--- src/app/components/Pubilc/Footer/index.tsx
+++ src/app/components/Pubilc/Footer/index.tsx
@@ -63,7 +63,7 @@ export default function Footer() {
 
 							return (
 								<a key={index} href={link.link} target="_blank" rel="noopener noreferrer">
-									<Icon size={24} color="#777">
+									<Icon size={24} color="#000000c1">
 										<IconComponent aria-label={link.name} />
 									</Icon>
 								</a>
