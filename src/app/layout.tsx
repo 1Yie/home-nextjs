6e6839686c73ca007f9f7f41fd98d2c355266d20
ichiyo <me@ichiyo.in>
2025-03-22 20:19:42 +0800

    feat: 为 ico 增加深色模式优化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="zh">
 			<head>
 				<meta charSet="utf-8" />
-				<link rel="icon" type="image/svg+xml" href="/logo.svg" />
+				<link rel="icon" type="image/svg+xml" href="/logo_light.svg" media="(prefers-color-scheme: light)"></link>
+				<link rel="icon" type="image/svg+xml" href="/logo_dark.svg" media="(prefers-color-scheme: dark)"></link>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="description" content="Hi there! I'm ichiyo. Welcome to my Homepage." />
 				<meta name="author" content="ichiyo" />
