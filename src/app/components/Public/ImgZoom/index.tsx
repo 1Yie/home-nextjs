3674490a1787213bc7914bc465b4161d00333e5e
ichiyo <me@ichiyo.in>
2025-06-06 17:36:35 +0800

    fix: 修复图片缩放组件当图片不存在时缩放会导致页面白屏问题

--- src/app/components/Public/ImgZoom/index.tsx
+++ src/app/components/Public/ImgZoom/index.tsx
@@ -5,15 +5,28 @@ import mediumZoom from 'medium-zoom';
 
 export default function ImageZoom() {
 	useEffect(() => {
-		const images = document.querySelectorAll('img[data-zoom="true"]');
-		images.forEach((img) => {
+		const imgs = document.querySelectorAll('img[data-zoom="true"]');
+		const readyImages: HTMLImageElement[] = [];
+
+		imgs.forEach((imgElement) => {
+			const img = imgElement as HTMLImageElement;
+
 			img.setAttribute('fetchpriority', 'low');
 			img.setAttribute('loading', 'lazy');
 			img.setAttribute('decoding', 'async');
+
+			if (img.complete && img.naturalWidth !== 0) {
+				readyImages.push(img);
+			} else {
+				img.addEventListener('load', () => {
+					mediumZoom(img, { margin: 12 });
+				});
+			}
 		});
-		mediumZoom(images, {
-			margin: 12,
-		});
+
+		if (readyImages.length > 0) {
+			mediumZoom(readyImages, { margin: 12 });
+		}
 	}, []);
 
 	return null;
