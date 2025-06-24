36b77edbc3d24b5f9c22c27f052a3fd047b9a0a4
ichiyo <me@ichiyo.in>
2025-06-24 18:39:31 +0800

    fix: 不使用dvh

--- src/app/components/Layout/Blog/PostList/index.tsx
+++ src/app/components/Layout/Blog/PostList/index.tsx
@@ -30,18 +30,18 @@ export default function BlogPage({ posts }: { posts: PostMetadata[] }) {
 		<>
 			<div className={style.rssLink}>
 				<section id={style.rssLinkPanel}>
-				<a href="/api/rss" aria-label="订阅RSS" className={style.rssIcon}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-						<path d="M19 20.001C19 11.729 12.271 5 4 5v2c7.168 0 13 5.832 13 13.001h2zm-7 0c0-4.962-4.037-9-9-9v2c3.86 0 7 3.14 7 7h2zm-5 0c0-2.761-2.239-5-5-5v2c1.657 0 3 1.344 3 3h2z" />
-					</svg>
-					RSS 订阅
-				</a>
-				<Link href="/tags" className={style.tagsLink}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-						<path d="M17.63 5.84C17.27 5.33 16.67 5 16 5L5 5.01C3.9 5.01 3 5.9 3 7v10c0 1.1.9 1.99 2 1.99L16 19c.67 0 1.27-.33 1.63-.84L22 12l-4.37-6.16zM16 17H5V7h11l3.55 5L16 17z" />
-					</svg>
-					Tags
-				</Link>
+					<a href="/api/rss" aria-label="订阅RSS" className={style.rssIcon}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+							<path d="M19 20.001C19 11.729 12.271 5 4 5v2c7.168 0 13 5.832 13 13.001h2zm-7 0c0-4.962-4.037-9-9-9v2c3.86 0 7 3.14 7 7h2zm-5 0c0-2.761-2.239-5-5-5v2c1.657 0 3 1.344 3 3h2z" />
+						</svg>
+						RSS 订阅
+					</a>
+					<Link href="/tags" className={style.tagsLink}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+							<path d="M17.63 5.84C17.27 5.33 16.67 5 16 5L5 5.01C3.9 5.01 3 5.9 3 7v10c0 1.1.9 1.99 2 1.99L16 19c.67 0 1.27-.33 1.63-.84L22 12l-4.37-6.16zM16 17H5V7h11l3.55 5L16 17z" />
+						</svg>
+						Tags
+					</Link>
 				</section>
 			</div>
 			<div className={style.blogContainer}>
